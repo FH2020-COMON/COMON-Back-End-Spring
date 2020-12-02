623759079099db1887c7ce691f8efe35a7381dff
kjbin0420 <kjbin0420@naver.com>
2020-12-02 19:39:57 +0900

    [Fix] AuthService Path

---
+++ src/main/java/com/on/spring/controller/company/CompanyController.java
@@ -0,0 +1,12 @@
+package com.on.spring.controller.company;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/company")
+@RequiredArgsConstructor
+public class CompanyController {
+
+}
---
+++ src/main/java/com/on/spring/service/company/CompanyService.java
@@ -0,0 +1,4 @@
+package com.on.spring.service.company;
+
+public interface CompanyService {
+}
