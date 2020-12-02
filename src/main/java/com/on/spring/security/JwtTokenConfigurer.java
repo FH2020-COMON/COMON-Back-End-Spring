61f55bab881a8101172e0497ad64b51c3e7a4b91
kjbin0420 <kjbin0420@naver.com>
2020-12-02 19:27:13 +0900

    [ADD] Authentication and Authorization

--- src/main/java/com/on/spring/security/JwtTokenConfigurer.java
+++ src/main/java/com/on/spring/security/JwtTokenConfigurer.java
@@ -1,4 +1,18 @@
 package com.on.spring.security;
 
-public class JwtTokenConfigurer {
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class JwtTokenConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) {
+        JwtTokenFilter tokenFilter = new JwtTokenFilter(jwtTokenProvider);
+        httpSecurity.addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
 }
--- src/main/java/com/on/spring/service/AuthService.java
+++ src/main/java/com/on/spring/service/AuthService.java
@@ -1,9 +1,10 @@
 package com.on.spring.service;
 
 import com.on.spring.payload.request.LoginRequest;
+import com.on.spring.payload.response.AccessTokenResponse;
 import com.on.spring.payload.response.TokenResponse;
 
 public interface AuthService {
-    public void login(LoginRequest request);
-    public TokenResponse tokenRefresh(String refreshToken);
+    public TokenResponse login(LoginRequest request);
+    public AccessTokenResponse tokenRefresh(String refreshToken);
 }
