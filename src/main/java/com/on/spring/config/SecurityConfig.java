658d1cefb7aa8f35e7f95a784ae5f2250e673b37
kjbin0420 <kjbin0420@naver.com>
2020-12-05 21:54:30 +0900

    [PATCH] Security Config

--- src/main/java/com/on/spring/config/SecurityConfig.java
+++ src/main/java/com/on/spring/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.on.spring.config;
 
+import com.on.spring.security.JwtTokenConfigurer;
+import com.on.spring.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,7 +14,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -19,7 +28,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
                 .sessionManagement().disable()
                 .formLogin().disable()
                 .authorizeRequests()
-                    .antMatchers("/**").permitAll();
+                    .antMatchers("/**").permitAll()
+                .and()
+                .apply(new JwtTokenConfigurer(jwtTokenProvider));
     }
 
     @Override
