de468283706f803d2008d4a7027f403d7f2b260c
kjbin0420 <kjbin0420@naver.com>
2020-12-06 07:08:27 +0900

    [ADD] CrowdResponse

--- src/main/java/com/on/spring/payload/response/CrowdResponse.java
+++ src/main/java/com/on/spring/payload/response/CrowdResponse.java
@@ -3,15 +3,12 @@ package com.on.spring.payload.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @AllArgsConstructor @NoArgsConstructor @Builder
 public class CrowdResponse {
     private String companyName;
     private String crowdTitle;
-    private int nowAmount;
-    private int destinationAmount;
     private String hashTag;
+    private Integer nowAmount;
+    private Integer destinationAmount;
 }
