40d86511deefa221109ef1bf5454e1d60cec7122
kjbin0420 <kjbin0420@naver.com>
2020-12-06 00:14:13 +0900

    [PATCH] CrowdController

--- src/main/java/com/on/spring/controller/CrowdController.java
+++ src/main/java/com/on/spring/controller/CrowdController.java
@@ -1,5 +1,9 @@
 package com.on.spring.controller;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.on.spring.exception.FileUploadFailedException;
 import com.on.spring.payload.request.UploadCrowdRequest;
 import com.on.spring.payload.response.CrowdListResponse;
 import com.on.spring.payload.response.CrowdResponse;
@@ -9,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -23,7 +28,14 @@ public class CrowdController {
     }
 
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-    public void uploadCrowd(@RequestPart("user") UploadCrowdRequest request, @RequestParam("files") List<MultipartFile> files) {
+    public void uploadCrowd(@RequestPart("user") String user, @RequestParam("files") List<MultipartFile> files) {
+        UploadCrowdRequest request = new UploadCrowdRequest();
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            request = objectMapper.readValue(user, UploadCrowdRequest.class);
+        } catch (IOException err) {
+            throw new FileUploadFailedException();
+        }
 
         crowdService.uploadCrowd(request, files);
     }
