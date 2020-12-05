739eb96b905962bb14ba207ae537a23220325836
kjbin0420 <kjbin0420@naver.com>
2020-12-05 17:11:31 +0900

    [ADD] Not Found

--- src/main/java/com/on/spring/exception/CompanyNotFoundException.java
+++ src/main/java/com/on/spring/exception/CompanyNotFoundException.java
@@ -1,8 +1,9 @@
 package com.on.spring.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(reason = "Company Not Found!!")
+@ResponseStatus(reason = "Company Not Found!!", value = HttpStatus.NOT_FOUND)
 public class CompanyNotFoundException extends RuntimeException {
     public CompanyNotFoundException() {
         super("Company Not Found!!");
