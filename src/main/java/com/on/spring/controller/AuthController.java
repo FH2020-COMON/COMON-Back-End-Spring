4d5f939dc77c8e7dfc52168d0fa7ef01fbb7b677
kjbin0420 <kjbin0420@naver.com>
2020-12-05 19:55:17 +0900

    [PATCH] AuthController

--- src/main/java/com/on/spring/controller/AuthController.java
+++ src/main/java/com/on/spring/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.on.spring.controller;
 
 import com.on.spring.payload.request.LoginRequest;
+import com.on.spring.security.JwtTokenProvider;
 import com.on.spring.service.auth.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -12,10 +13,16 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping
     public String signIn(@RequestBody @Valid LoginRequest request) {
         System.out.println("email : " + request.getEmail() + "password : " + request.getPassword());
         return authService.login(request);
+    }
+
+    @GetMapping("/{accessToken}")
+    public String getUserEmail(@PathVariable String accessToken) {
+        return jwtTokenProvider.getEmail(accessToken);
     }
 }
